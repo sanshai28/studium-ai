75a4a403a1e0bceac0f3e9297d67b5f90473e085
sanshai28 <sanshai28@gmail.com>
2026-01-24 17:50:02 +0800

    front end init

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to Studium AI</h1>
+      <p>Your intelligent note-taking companion</p>
+    </div>
+  )
+}
+
+export default Home
